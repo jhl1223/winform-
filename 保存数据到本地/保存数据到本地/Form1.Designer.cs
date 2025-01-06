5a3e9dbf353f0b53f46dc2db534a74814a701a3e
jhl1223 <130202702+jhl1223@users.noreply.github.com>
2025-01-06 11:12:57 +0800

    Add files via upload

---
+++ 保存数据到本地/保存数据到本地/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace 保存数据到本地
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.save = new System.Windows.Forms.Button();
+            this.get = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.get2 = new System.Windows.Forms.Button();
+            this.get3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.save4 = new System.Windows.Forms.Button();
+            this.get4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(16, 24);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(122, 23);
+            this.save.TabIndex = 0;
+            this.save.Text = "method1";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // get
+            // 
+            this.get.Location = new System.Drawing.Point(173, 8);
+            this.get.Name = "get";
+            this.get.Size = new System.Drawing.Size(126, 29);
+            this.get.TabIndex = 1;
+            this.get.Text = "文件系统获取";
+            this.get.UseVisualStyleBackColor = true;
+            this.get.Click += new System.EventHandler(this.get_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 25);
+            this.textBox3.TabIndex = 4;
+            // 
+            // get2
+            // 
+            this.get2.Location = new System.Drawing.Point(173, 43);
+            this.get2.Name = "get2";
+            this.get2.Size = new System.Drawing.Size(126, 29);
+            this.get2.TabIndex = 5;
+            this.get2.Text = "注册表获取";
+            this.get2.UseVisualStyleBackColor = true;
+            this.get2.Click += new System.EventHandler(this.get2_Click);
+            // 
+            // get3
+            // 
+            this.get3.Location = new System.Drawing.Point(173, 78);
+            this.get3.Name = "get3";
+            this.get3.Size = new System.Drawing.Size(126, 29);
+            this.get3.TabIndex = 6;
+            this.get3.Text = "缓存获取";
+            this.get3.UseVisualStyleBackColor = true;
+            this.get3.Click += new System.EventHandler(this.get3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(122, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "method2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "method3";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.save4);
+            this.groupBox1.Controls.Add(this.save);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 105);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 173);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "save";
+            // 
+            // save4
+            // 
+            this.save4.Location = new System.Drawing.Point(16, 111);
+            this.save4.Name = "save4";
+            this.save4.Size = new System.Drawing.Size(122, 23);
+            this.save4.TabIndex = 10;
+            this.save4.Text = "method4";
+            this.save4.UseVisualStyleBackColor = true;
+            this.save4.Click += new System.EventHandler(this.save4_Click);
+            // 
+            // get4
+            // 
+            this.get4.Location = new System.Drawing.Point(173, 113);
+            this.get4.Name = "get4";
+            this.get4.Size = new System.Drawing.Size(126, 29);
+            this.get4.TabIndex = 10;
+            this.get4.Text = "多文件系统获取";
+            this.get4.UseVisualStyleBackColor = true;
+            this.get4.Click += new System.EventHandler(this.get4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.get4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.get3);
+            this.Controls.Add(this.get2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.get);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.Button get;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button get2;
+        private System.Windows.Forms.Button get3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button save4;
+        private System.Windows.Forms.Button get4;
+    }
+}
+
---
+++ 保存数据到本地/保存数据到本地/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+using Microsoft.Win32;  // 引入注册表相关的命名空间
+using System.Runtime.Caching;
+
+namespace 保存数据到本地
+{
+    public partial class Form1 : Form
+    {
+        // 缓存文件路径
+        private string cacheFilePath = "cache.json";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 保存数据到文件
+        private void save_Click(object sender, EventArgs e)
+        {
+            // 获取 TextBox 中的内容
+            string textBoxContent = textBox1.Text;
+
+            // 将 TextBox 内容序列化为 JSON 格式
+            string jsonContent = JsonConvert.SerializeObject(textBoxContent);
+
+            // 将 JSON 内容保存到文件
+            File.WriteAllText(cacheFilePath, jsonContent);
+
+            //MessageBox.Show("数据已保存！", "保存成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // 从文件获取数据并加载到 TextBox
+        private void get_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(cacheFilePath))
+            {
+                // 读取文件中的 JSON 内容
+                string jsonContent = File.ReadAllText(cacheFilePath);
+
+                // 反序列化 JSON 内容到 TextBox
+                string restoredText = JsonConvert.DeserializeObject<string>(jsonContent);
+
+                // 将读取的数据设置回 TextBox
+                textBox1.Text = restoredText;
+
+                //MessageBox.Show("数据已加载！", "加载成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("缓存文件不存在！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //注册表保存
+        // 注册表键名 (此处假设你将数据保存在软件的 "MyApp" 分支下)
+        private string registryKeyPath = @"Software\MyApp";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string textBoxContent = textBox1.Text;
+
+            // 打开或创建注册表项
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(registryKeyPath))
+            {
+                if (key != null)
+                {
+                    // 将 TextBox 内容保存到注册表中
+                    key.SetValue("TextBoxContent", textBoxContent);
+                    //MessageBox.Show("数据已保存到注册表！", "保存成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void get2_Click(object sender, EventArgs e)
+        {
+            // 打开注册表项
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(registryKeyPath))
+            {
+                if (key != null)
+                {
+                    // 从注册表中读取 TextBox 内容
+                    object savedValue = key.GetValue("TextBoxContent");
+                    if (savedValue != null)
+                    {
+                        // 设置 TextBox 的内容为读取到的数据
+                        textBox1.Text = savedValue.ToString();
+                        //MessageBox.Show("数据已从注册表加载！", "加载成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("注册表中没有保存的数据！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("注册表项不存在！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        // 使用MemoryCache来保存数据
+        private static MemoryCache memoryCache = MemoryCache.Default;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 将TextBox中的数据保存到MemoryCache
+            string key = "textBoxData";  // 设置缓存的键
+            string value = textBox1.Text; // 获取TextBox中的内容
+
+            // 使用Set方法保存数据，第二个参数是过期时间，这里设置为60分钟
+            memoryCache.Set(key, value, DateTimeOffset.Now.AddMinutes(60));
+            //MessageBox.Show("数据已保存到内存缓存！");
+        }
+
+        private void get3_Click(object sender, EventArgs e)
+        {
+            // 从MemoryCache中获取数据
+            string key = "textBoxData";  // 假设键是固定的
+            string cachedValue = memoryCache.Get(key) as string;  // 获取缓存数据
+
+            if (!string.IsNullOrEmpty(cachedValue))
+            {
+                textBox1.Text = cachedValue; // 显示缓存中的数据
+            }
+            else
+            {
+                MessageBox.Show("没有找到缓存数据！");
+            }
+        }
+
+        //多文件系统获取
+        // 缓存文件路径
+        private string cacheFilePath2 = "cache2.json";
+        private void save4_Click(object sender, EventArgs e)
+        {
+            var textData = new Dictionary<string, string>
+            {
+                { "textBox1", textBox1.Text },
+                { "textBox2", textBox2.Text }
+            };
+
+            // 序列化字典为 JSON
+            string jsonContent = JsonConvert.SerializeObject(textData);
+
+            // 将 JSON 内容保存到文件
+            File.WriteAllText(cacheFilePath2, jsonContent);
+
+            //MessageBox.Show("数据已保存！", "保存成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void get4_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(cacheFilePath2))
+            {
+                // 读取文件中的 JSON 内容
+                string jsonContent = File.ReadAllText(cacheFilePath2);
+
+                // 反序列化 JSON 内容到 Dictionary<string, string>
+                var restoredData = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonContent);
+
+                // 判断反序列化是否成功，并将数据设置回 TextBox
+                if (restoredData != null)
+                {
+                    if (restoredData.ContainsKey("textBox1"))
+                        textBox1.Text = restoredData["textBox1"];
+                    if (restoredData.ContainsKey("textBox2"))
+                        textBox2.Text = restoredData["textBox2"];
+
+                    //MessageBox.Show("数据已加载！", "加载成功", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("加载数据失败！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("缓存文件不存在！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //
+
+
+    }
+}
